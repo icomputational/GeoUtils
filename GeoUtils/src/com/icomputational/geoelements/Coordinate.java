b410f751d8eda3349f99447c04b4534e0629374b
icomputational <itopcoder@gmail.com>
2014-09-14 09:51:15 +0000

    Create first file to verify git function

---
+++ GeoUtils/src/com/icomputational/geoelements/Coordinate.java
@@ -0,0 +1,11 @@
+package com.icomputational.geoelements;
+
+public class Coordinate implements Comparable<Coordinate> {
+
+	@Override
+	public int compareTo(Coordinate o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
